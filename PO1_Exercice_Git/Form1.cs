488a5043c62f26a8638ce011a75f23f843ea2b99
AGagnon <41961082+Agagnon01@users.noreply.github.com>
2024-01-15 16:06:38 -0500

    Ajustement et ajout du tableau

--- PO1_Exercice_Git/Form1.cs
+++ PO1_Exercice_Git/Form1.cs
@@ -2,6 +2,8 @@ namespace PO1_Exercice_Git
 {
     public partial class Form1 : Form
     {
+        int[] tableau = [1, 2, 3, 5, 8, 13];
+
         public Form1()
         {
             InitializeComponent();
