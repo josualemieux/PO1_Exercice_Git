a346d074462e9645fd4d350a14ed4086e00f09bf
AGagnon <41961082+Agagnon01@users.noreply.github.com>
2024-01-15 16:27:14 -0500

    Changement de Jean

--- PO1_Exercice_Git/Form1.cs
+++ PO1_Exercice_Git/Form1.cs
@@ -11,12 +11,60 @@ namespace PO1_Exercice_Git
 
         private void button_Somme_Click(object sender, EventArgs e)
         {
-
+            int somme = 0;
+            for (int i = 0; i < tableau.Length; i++)
+            {
+                somme = tableau[i];
+            }
+            MessageBox.Show("La somme des élément du tableau est : " + somme);
         }
 
         private void button_TestPremier_Click(object sender, EventArgs e)
         {
 
+            if(int.TryParse(textBox_Nombre.Text, out int nombre))
+            {
+                if (nombre < 2)
+                {
+                    MessageBox.Show("Le nombre doit être supérieur ou égal à 2");
+                }
+                else
+                {
+                    bool estPremier = true;
+                    for (int i = 2; i < nombre; i++)
+                    {
+                        if (nombre % i == 0)
+                        {
+                            estPremier = false;
+                            break;
+                        }
+                    }
+                    if (estPremier)
+                    {
+                        MessageBox.Show("Le nombre est premier");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Le nombre n'est pas premier");
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Le nombre doit être un entier");
+            }
+        }
+
+        private void button_PlusGrand_Click(object sender, EventArgs e)
+        {
+            int somme = 0;
+            for (int i = 0; i < tableau.Length; i++)
+            {
+                if (tableau[i] > somme)
+                {
+                    somme = tableau[i];
+                }
+            }
         }
     }
 }
